aabc17460723594fbc7c2b51db0d625d8b49992f
Esraa Soliman <ee341110@gmail.com>
2025-12-05 21:21:50 +0200

    Add Machines Page

--- src/api/modules/machinesApi.js
+++ src/api/modules/machinesApi.js
@@ -1,5 +1,7 @@
+// src/api/modules/machinesApi.js
 import { httpClient } from "../httpClient";
 
+// GET /machine/?page=1&page_size=10&all=false
 export function getMachines({ page, page_size, all = false }) {
   const queryParams = all
     ? "all=true"
@@ -8,14 +10,17 @@ export function getMachines({ page, page_size, all = false }) {
   return httpClient.get(`/machine/?${queryParams}`);
 }
 
+// POST /machine/
 export function addMachine(newMachine) {
   return httpClient.post("/machine/", newMachine);
 }
 
+// PUT /machine/:id
 export function updateMachine({ id, ...patch }) {
   return httpClient.put(`/machine/${id}`, patch);
 }
 
+// DELETE /machine/:id
 export function deleteMachine(id) {
   return httpClient.delete(`/machine/${id}`);
 }
